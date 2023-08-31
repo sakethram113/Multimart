7461915f018cfa9bf6c4bacc0b998554620b8f09
sakethram113 <rampodicheti1234@gmail.com>
2023-08-31 10:21:42 +0530

    changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,21 @@ import logo from '../../assets/images/eco-logo.png'
 import userIcon from '../../assets/images/user-icon.png'
 import {Container, Row} from 'reactstrap';
 
+const nav__links =[
+  {
+    path:'home',
+    display:'Home'
+  },
+  {
+    path:'shop',
+    display:'Shop'
+  },
+  {
+    path:'cart',
+    display:'Cart'
+  },
+]
+
 
 const Header = () => {
   return <header className="header">
@@ -15,20 +30,16 @@ const Header = () => {
             <img src={logo} alt="logo" />
             <div>
               <h1>Multimart</h1>
-              <p>Since 1951</p>
             </div>
           </div>
           <div className="navigation">
             <ul className="menu">
-              <li className="nav__menu">
-                 <NavLink>Home</NavLink>
-              </li>
-              <li className="nav__menu">
-                 <NavLink>Shop</NavLink>
-              </li>
-              <li className="nav__menu">
-                 <NavLink>Cart</NavLink>
-              </li>
+             {
+              nav__links.map((item,index) =>(
+                <li className="nav__item" key={index}>
+                <NavLink to={item.path} className={(navClass=>navClass.isActive ? 'nav__active' : '')}>{item.display}</NavLink>
+             </li>
+              ))}
             </ul>
           </div>
 
