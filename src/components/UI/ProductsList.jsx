cf3f0940eb18fef0a03133f3b43cde5626659666
sakethram113 <rampodicheti1234@gmail.com>
2023-09-01 10:28:32 +0530

    countdown updated and new arrivals

--- src/components/UI/ProductsList.jsx
+++ src/components/UI/ProductsList.jsx
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 const ProductsList = ({data}) => {
   return (
-   <div className='d-flex'>
+   <div className='d-flex flex-wrap'>
     {
         data?.map(item=>(
           <ProductCard item={item} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,9 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Services from '../services/Services';
 import ProductsList from '../components/UI/ProductsList';
-import products from '../assets/data/products'
+import products from '../assets/data/products';
+import counterImg from '../assets/images/counter-timer-img.png';
+import Clock from '../components/UI/Clock';
 
 const Home = () => {
 
@@ -15,14 +17,22 @@ const Home = () => {
 
 const [trendingProducts, setTrendingProducts] = useState([]);
 const [bestSalesProducts, setBestSalesProducts] = useState([]);
+const [mobileProducts, setMobileProducts] = useState([]);
+const [wirelessProducts, setWirelessProducts] = useState([]);
 useEffect(()=>{
   const filteredTrendingProducts = products.filter
   (item =>item.category === 'chair');
-  const filteredbestSalesProducts = products.filter
+  const filteredBestSalesProducts = products.filter
   (item =>item.category === 'sofa');
+  const filteredMobileProducts = products.filter
+  (item =>item.category === 'mobile');
+  const filteredWirelessProducts = products.filter
+  (item =>item.category === 'wireless');
 
   setTrendingProducts(filteredTrendingProducts);
-  setBestSalesProducts(filteredbestSalesProducts);
+  setBestSalesProducts(filteredBestSalesProducts);
+  setMobileProducts(filteredMobileProducts);
+  setWirelessProducts(filteredWirelessProducts);
 }, []);
 
 
@@ -66,11 +76,43 @@ useEffect(()=>{
 
     <section className="best__sales">
       <Container>
-      <Row>
+       <Row>
           <Col lg='12' className='text-center'>
             <h2 className='section__title'>Best Sales</h2>
-          <ProductsList data={bestSalesProducts}/>
           </Col>
+          <ProductsList data={bestSalesProducts}/>
+        </Row>
+      </Container>
+    </section>
+
+    <section className="timer__count">
+      <Container>
+        <Row>
+          <Col lg='6' md='6'>
+            <div className="clock__top-content">
+              <h4 className='text-white fs-6 mb-2'>Limited Offers</h4>
+              <h3 className='text-white fs-5 mb-3'>Quality AramChair</h3>
+            </div>
+            <Clock />
+
+            <motion.button whileTap={{scale:1.2}} className="buy__btn store__btn"><Link to='/'>Visit Store</Link></motion.button>
+          </Col>
+
+          <Col lg='6' md='6' className='text-end'>
+            <img src={counterImg} alt="" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section className="new__arrivals">
+      <Container>
+        <Row>
+          <Col lg='12' className='text-center'>
+            <h2 className="section__title">New Arrivals</h2>
+          </Col>
+          <ProductsList data={mobileProducts}/>
+          <ProductsList data={wirelessProducts}/>
         </Row>
       </Container>
     </section>
